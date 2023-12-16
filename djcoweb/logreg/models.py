846b4b9050ccc2538632ce53b0ce3a6064c464fe
KiSukLee <needcompsci@gmail.com>
2023-12-15 16:36:06 -0800

    12.15.23 work in office

--- djcoweb/logreg/models.py
+++ djcoweb/logreg/models.py
@@ -1,11 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class UserManager(models.Manager):
+    def validate(self, postData):
+        errors = {}
+        if postData['submit'] == 'create':
+            if postData['name'] <= 3:
+                errors["name"] = 'Name must be at least 3 characters long'
+            if postData['email']:
+                errors['email'] = 'Invalid email'
+            if len(postData['number']) != 10:
+                errors['number'] = 'Invalid phone number'
+            if postData['pword'] < 8:
+                errors['pword'] = 'Password must be at least 8 characters'
+            return errors
+        else:
+            if User.objects.get['email'] != postData['name']:
+                errors['email'] = "User does not exist"
+            if User.objects.get['pword'] != postData['pword']:
+                errors['pword'] = "Incorrect password"
+            return errors
 class User(models.Model):
     name = models.CharField(max_length = 50)
     email = models.CharField(max_length = 100)
     number = models.IntegerField()
     password = models.CharField(max_length = 200)
+    objects = UserManager()
 
     def __str__(self):
         return self.name
--- djcoweb/logreg/views.py
+++ djcoweb/logreg/views.py
@@ -1,40 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
+from django.contrib import messages
 from . models import User
 
 # Create your views here.
 def logreg(request):
     return render(request, "logreg/logreg.html")
-def create(request):
-    if validate(request.post):
-        return redirect('dashboard')
-    return redirect('/')
-def match(request):
-    if validate(request.post):
-        return redirect('dashboard')
-    return redirect('/')
-def validate(request, context):
-    if context[submit] == 'create':
-        value = False
-        if context['name'] <= 3:
-            flash('Name must be at least 3 characters long')
-        if context['email']:
-            flash('Invalid email')
-        if len(context['number']) != 10:
-            flash('Invalid phone number')
-        if context['pword'] < 8:
-            flash('Password must be at least 8 characters')
-        else:
-            value = True
-        return value
-    else:
-        value = False
-        if User.objects.get['email'] != context['name']:
-            flash("User does not exist")
-        if User.objects.get['pword'] != context['pword']:
-            flash("Incorrect password")
-        else:
-            value = True
-        return value
+def validate(request):
+    errors = User.objects.validate(request.POST)
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/')
+    return redirect()
 def dashboard(request, session):
     return render(request, "logreg/dashboard.html")
