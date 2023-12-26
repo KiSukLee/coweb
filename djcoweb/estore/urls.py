9766cabbaaa4af0690782a0420943e0f7874a39e
KiSukLee <needcompsci@gmail.com>
2023-12-26 14:16:58 -0800

    12.26.23 added functioning cart page and made additional changes to existing features

--- djcoweb/estore/urls.py
+++ djcoweb/estore/urls.py
@@ -6,6 +6,7 @@ app_name = "estore"
 urlpatterns = [
     path("about", views.about),
     path("products", views.products),
-    path("<str:action>/<int:product_id>", views.modify_cart),
-    path("cart", views.cart)
+    path("<str:method>/<str:action>/<int:product_id>", views.modify_cart),
+    path("cart", views.cart),
+    path("checkout", views.checkout)
 ]
--- djcoweb/estore/views.py
+++ djcoweb/estore/views.py
@@ -10,18 +10,23 @@ def about(request):
 def products(request):
     return render(request, "estore/inventory.html", {"products":Inventory.objects.all()})
 
-def modify_cart(request, action, product_id):
+def prod_cart(method):
+    if method == "products":
+        return redirect("/estore/products")
+    return redirect("/estore/cart")
+
+def modify_cart(request, method, action, product_id):
     inventory = Inventory.objects.get(id = product_id)
     this_user = User.objects.get(email = User.objects.get(name = request.session['name']).email)
-    this_cart = Cart.objects.get(user = this_user, id = request.session["cart_id"])
+    this_cart = Cart.objects.get(id = request.session["cart_id"], user = this_user)
     
-    print(this_user.name)
+    print(this_cart.user.name)
     print(this_cart.id)
     #Add to Cart
     if action == "add":
         if not inventory.quantity > 0:
             messages.error(request, "Out of stock")
-            return redirect("/estore/products")
+            return prod_cart(method)
         inventory.quantity -= 1
         inventory.save()
         if this_cart.products.filter(product_id = product_id):
@@ -37,18 +42,18 @@ def modify_cart(request, action, product_id):
         print(this_cart.total)
         print(this_cart.products.get(product_id = product_id).product_id)
         print(this_cart.products.get(product_id = product_id).quantity)
-        return redirect("/estore/products")
+        return prod_cart(method)
     
     #Remove from Cart
     else:
         if not this_cart.products.filter(product_id = product_id):
             messages.error(request, "Insufficient amount")
-            return redirect("/estore/products")
+            return prod_cart(method)
         this_product = this_cart.products.get(product_id = product_id)
         this_product.quantity -= 1
         this_product.save()
         print(this_product.product_id)
-        print(this_cart.quantity)
+        print(this_product.quantity)
         if this_product.quantity < 1:
             print(this_product.quantity)
             this_cart.products.get(product_id = product_id).delete()
@@ -57,7 +62,21 @@ def modify_cart(request, action, product_id):
         print(this_cart.total)
         inventory.quantity += 1
         inventory.save()
-        return redirect("/estore/products")
+        return prod_cart(method)
 
 def cart(request):
-    return render(request, "estore/cart.html")
+    cart = Cart.objects.get(id = request.session['cart_id'])
+    products = cart.products.all()
+    prods = []
+    for product in products:
+        prod = {
+            "prod_id":product.product_id,
+            "name":Inventory.objects.get(id = product.product_id).name,
+            "price":Inventory.objects.get(id = product.product_id).price,
+            "quantity":product.quantity
+        }
+        prods.append(prod)
+    print(prods)
+    return render(request, "estore/cart.html", context = {"prods":prods, "total":cart.total})
+def checkout(request):
+    
